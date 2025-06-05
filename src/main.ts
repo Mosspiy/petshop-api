c113d1f7ae5779500403d54d41834eea6321c208
Mosspiy <165090848+Mosspiy@users.noreply.github.com>
2025-06-05 20:33:07 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
