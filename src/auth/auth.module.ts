aed50f803593a9505775d890f4443e7a98b47801
Mosspiy <165090848+Mosspiy@users.noreply.github.com>
2025-06-05 21:42:30 +0700

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
+// auth.module.ts
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
@@ -13,14 +15,16 @@ import { LineService } from './line.service';
   imports: [
     HttpModule,
     UsersModule,
+    ConfigModule, 
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
-  imports: [ConfigModule],
-  inject: [ConfigService],
-  useFactory: async (config: ConfigService) => ({
-    secret: config.get('JWT_SECRET'),
-    signOptions: { expiresIn: '24h' },
-  }),
+      imports: [ConfigModule],
+      inject: [ConfigService], 
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, LineStrategy, LineService],
